49159f4991b79d5fe5117810cb48642a98e2db26
Manoj Hankare <71174622+Manojhankare@users.noreply.github.com>
2023-03-26 08:58:31 +0530

    Simple Java Program

---
+++ 01 Simple Java Program/SimpleJavaProgram.java
@@ -0,0 +1,13 @@
+package a01_Simple_Java_Program;
+
+public class SimpleJavaProgram {
+
+	
+//	Printing the String "hello World"
+	
+	public static void main(String[] args) {
+	  
+		System.out.println("Hello World");
+	}
+
+}
