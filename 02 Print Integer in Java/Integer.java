bd0337c3bbf20ed74e0f8d99a99c376794456cb9
Manoj Hankare <71174622+Manojhankare@users.noreply.github.com>
2023-03-30 16:50:33 +0530

    Printing Integers

---
+++ 02 Print Integer in Java/Integer.java
@@ -0,0 +1,20 @@
+//package a02_print_Integer_Java;
+
+public class Integer {
+//	Printing integer
+	public static void main(String[] args) {
+
+		int m = 45; // declaring Variable with int datatype and value
+		int n; // declaring variable
+
+		System.out.println("Value of m is : " + m); // Printing variable m
+		System.out.println("");
+
+//Printing Variable n using for loop which will initialize it's value from 1 - 10
+
+		for (n = 1; n <= 10; n++) {
+
+			System.out.println("Value of n is : " + n);
+		}
+	}
+}
