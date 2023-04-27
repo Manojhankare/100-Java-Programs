7a70e2c42f174fe9f81d7255ca5ef7454adf01a6
Manoj Hankare <71174622+Manojhankare@users.noreply.github.com>
2023-04-27 10:58:45 +0530

    For Loop

---
+++ 12 For Loop/ForLoop.java
@@ -0,0 +1,16 @@
+package aa12_For_Loop;
+
+public class ForLoop {
+	// Simple For Loop demo
+
+	public static void main(String[] args) {
+		int i;
+
+		for (i = 101; i <= 10; i++) {
+			System.out.println(i);// printing no ..
+		}
+		for (i = 101; i >= 10; i--) {
+			System.out.println(i);// printing no in reverse..
+		}
+	}
+}
